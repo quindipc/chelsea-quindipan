9033f07bee67ad5e9a6c0927fd863dbddc6ec06f
Chelsea Quindipan <quindipc@gmail.com>
2023-10-30 10:34:19 -0400

    add chair and environment

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
 
@@ -6,17 +6,29 @@ export const Experience = () => {
   const { animation } = useControls({
     animation: {
       value: "Typing",
-      options: ["Typing", "Falling", "Standing"]
-    }
-  })
+      options: ["Typing", "Falling", "Standing"],
+    },
+  });
 
   return (
     <>
       <OrbitControls />
+      <Sky />
+      <Environment preset="sunset" />
       <group position-y={-1}>
         <Avatar animation={animation} />
+        {animation === "Typing" && (
+      <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+        <boxGeometry />
+        <meshStandardMaterial color="#fff" />
+      </mesh>
+)}
+
+      <mesh scale={5} rotation-x={-Math.PI * 0.5}>
+        <planeGeometry />
+        <meshStandardMaterial color="#fff" />
+      </mesh>
       </group>
-    <ambientLight intensity={1} />
     </>
   );
 };
