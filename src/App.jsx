176b49be3fd5422bd2eb18e82305650c818b5b11
Chelsea Quindipan <quindipc@gmail.com>
2023-10-30 16:47:02 -0400

    add tailwind dependency

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 
 function App() {
-  return (
-    <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+  return (<>
+     <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+    <color attach="background" args={["#ececec"]} />
+   <Experience />
+   </Canvas>
+  </>
   );
 }
 
