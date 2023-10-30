c2f67ea1052713be1b33ca2c4646da7c0c7342e8
Chelsea Quindipan <quindipc@gmail.com>
2023-10-30 10:39:18 -0400

    add shadows

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Environment, OrbitControls, Sky, ContactShadows } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
 
@@ -16,18 +16,27 @@ export const Experience = () => {
       <Sky />
       <Environment preset="sunset" />
       <group position-y={-1}>
+        <ContactShadows
+          opacity={0.42}
+          scale={10}
+          blur={1}
+          far={10}
+          resolution={256}
+          color="#000000"
+        />
         <Avatar animation={animation} />
         {animation === "Typing" && (
-      <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
-        <boxGeometry />
-        <meshStandardMaterial color="#fff" />
-      </mesh>
-)}
+          // Box where avatar is typing
+          <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+            <boxGeometry />
+            <meshStandardMaterial color="#fff" />
+          </mesh>
+        )}
 
-      <mesh scale={5} rotation-x={-Math.PI * 0.5}>
-        <planeGeometry />
-        <meshStandardMaterial color="#fff" />
-      </mesh>
+        <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+          <planeGeometry />
+          <meshStandardMaterial color="#fff" />
+        </mesh>
       </group>
     </>
   );
