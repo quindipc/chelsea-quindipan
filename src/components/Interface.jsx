be105f15c3052ef086a2788ea0562e457fb18ede
Chelsea Quindipan <quindipc@gmail.com>
2023-10-31 11:46:35 -0400

    add skills section

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,40 +1,105 @@
 const Section = (props) => {
   const { children } = props;
-  return <section className={`h-screen w-screen p-8 max-x-screen 2xl mx-auto flex flex-col items-start justify-center`}>{children}</section>;
+  return (
+    <section
+      className={`h-screen w-screen p-8 max-x-screen 2xl mx-auto flex flex-col items-start justify-center`}
+    >
+      {children}
+    </section>
+  );
 };
 
 export const Interface = () => {
-    return <div className="flex flex-col items-center w-screen">
-        <AboutSection/>
-        <Section>
-            <h1>Home</h1>
-        </Section>
-        <Section>
-            <h1>Skills</h1>
-        </Section>
-        <Section>
-            <h1>Project</h1>
-        </Section>
-        <Section>
-            <h1>Contact</h1>
-        </Section>
-  </div>;
+  return (
+    <div className="flex flex-col items-center w-screen">
+      <AboutSection />
+      <SkillsSection />
+      <Section>
+        <h1>Home</h1>
+      </Section>
+      <Section>
+        <h1>Skills</h1>
+      </Section>
+      <Section>
+        <h1>Project</h1>
+      </Section>
+      <Section>
+        <h1>Contact</h1>
+      </Section>
+    </div>
+  );
 };
 
 const AboutSection = () => {
-    return (
-        <Section>
-            <h1 className="text-6xl font-extrabold leading-snug">Hi I'm <br />  <span className="bg-primary px-1 italic">Chelsea Quindipan</span></h1>
+  return (
+    <Section>
+      <h1 className="text-6xl text-tertiary font-extrabold leading-snug">
+        Hi I'm <br />{" "}
+        <span className="text-white bg-primary py-1 px-1 italic rounded-lg">
+          Chelsea Quindipan
+        </span>
+      </h1>
 
-            <p className="text-lg text-gray-600 mt-"> I am a Graphic Designer turned Software Developer based in Toronto.
-                <br />
-                I love to build and create things! I am committed to creating
-                <br />
-                seamless, impactful digital solutions that bridge the gap between
-                <br />
-                technology and design.</p>
-            <button className={`bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16`}>Contact me</button>
+      <p className="text-lg text-black mt-6">
+        {" "}
+        I am a Graphic Designer turned Software Developer based in Toronto.
+        <br />
+        I love to build and create things! I am committed to creating
+        <br />
+        seamless, impactful digital solutions that bridge the gap between
+        <br />
+        technology and design.
+      </p>
+      <button
+        className={`bg-tertiary text-white py-4 px-8 rounded-lg font-bold text-lg mt-6`}
+      >
+        Contact me
+      </button>
+    </Section>
+  );
+};
 
-        </Section>
-    )
-}
+const skills = [
+  {
+    title: "React",
+    icon: "",
+  },
+  {
+    title: "MySQL",
+    icon: "",
+  },
+  {
+    title: "Nodejs",
+    icon: "",
+  },
+  {
+    title: "Express",
+    icon: "",
+  },
+  {
+    title: "TypeScript",
+    icon: "",
+  },
+  {
+    title: "Threejs / React Three Fiber",
+    icon: "",
+  },
+];
+
+const SkillsSection = () => {
+  return (
+    <Section>
+      <div>
+        <h2 className="text-5xl font-bold">Skills</h2>
+      </div>
+      <div className="mt-8 space-y-">
+        {skills.map((skill, index) => (
+          <div className="w-64" key={index}>
+            <h3 className="text-xl font-bold text-black">{skill.title}</h3>
+            <img src={skill.icon} alt={skill.title} className="w-64" />
+          </div>
+        ))}
+      </div>
+    </Section>
+  );
+};
