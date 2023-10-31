988271a586c1df21a40699bde71f7a1829ebd605
Chelsea Quindipan <quindipc@gmail.com>
2023-10-31 12:20:41 -0400

    Add contact section

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -14,18 +14,8 @@ export const Interface = () => {
     <div className="flex flex-col items-center w-screen">
       <AboutSection />
       <SkillsSection />
-      <Section>
-        <h1>Home</h1>
-      </Section>
-      <Section>
-        <h1>Skills</h1>
-      </Section>
-      <Section>
-        <h1>Project</h1>
-      </Section>
-      <Section>
-        <h1>Contact</h1>
-      </Section>
+      {/* Project */}
+      <ContactSection />
     </div>
   );
 };
@@ -35,7 +25,11 @@ const AboutSection = () => {
     <Section>
       <h1 className="text-6xl text-tertiary font-extrabold leading-snug">
         Hi I'm <br />{" "}
-              <img className="bg-primary" src="src/assets/logo/w-logo-cropped.svg" alt="white logo" />
+        <img
+          className="bg-primary"
+          src="src/assets/logo/w-logo-cropped.svg"
+          alt="white logo"
+        />
       </h1>
 
       <p className="text-lg text-black mt-6">
@@ -101,3 +95,52 @@ const SkillsSection = () => {
     </Section>
   );
 };
+
+
+const ContactSection = () => { 
+
+    return (
+        <Section>
+          <h2 className="text-5xl font-bold">Contact me</h2>
+          <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
+            <form>
+              <label for="name" className="font-medium text-gray-900 block mb-1">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+              />
+              <label
+                for="email"
+                className="font-medium text-gray-900 block mb-1 mt-8"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+              />
+              <label
+                for="email"
+                className="font-medium text-gray-900 block mb-1 mt-8"
+              >
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+              />
+              <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
+                Submit
+              </button>
+            </form>
+          </div>
+        </Section>
+      );
+}
