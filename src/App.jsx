0769783a3f6ed4b6ff61d585b0380ca719ef35f6
Chelsea Quindipan <quindipc@gmail.com>
2023-11-02 15:51:37 -0400

    add menu toggle

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,29 @@ import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { Interface } from "./components/Interface";
 import { ScrollManager } from "./components/ScrollManager";
+import { Menu } from "./components/Menu";
 
 function App() {
   const [section, setSection] = useState(0);
+  const [menuOpened, setMenuOpened] = useState(false);
 
   return (
     <>
-      <Canvas shadows camera={{ position: [0, 2, 5], fov: 70 }}>
-        <color attach="background" args={["#ececec"]} />
+      <Canvas shadows camera={{ position: [0, 3, 3], fov: 70 }}>
+        <color attach="background" args={["#e6e7ff"]} />
         <ScrollControls pages={4} damping={0.1}>
-        <ScrollManager section={section} onSectionChange={setSection} />
+          <ScrollManager section={section} onSectionChange={setSection} />
           <Experience />
           <Scroll html>
             <Interface />
           </Scroll>
         </ScrollControls>
       </Canvas>
+      <Menu
+        onSectionChange={setSection}
+        menuOpened={menuOpened}
+        setMenuOpened={setMenuOpened}
+      />
     </>
   );
 }
