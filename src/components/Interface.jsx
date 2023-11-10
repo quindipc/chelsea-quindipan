689e6ad4d8112a4d174660864c54d0c7e0d4a755
Chelsea Quindipan <quindipc@gmail.com>
2023-11-10 11:41:03 -0500

    edit sections

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -8,6 +8,9 @@ import typescriptIcon from "../assets/icons/typescript.png";
 import javascriptIcon from "../assets/icons/javascript.png";
 import tailwindIcon from "../assets/icons/tailwindcss.png";
 import SassIcon from "../assets/icons/sass.png";
+import linkedinIcon from "../assets/icons/linkedin.png";
+import githubIcon from "../assets/icons/github.png";
+import mailIcon from "../assets/icons/mail.png";
 
 const Section = (props) => {
   const { children } = props;
@@ -33,7 +36,8 @@ export const Interface = () => {
       <AboutSection />
       <SkillsSection />
       <ProjectSection />
-      <ContactSection />
+      <SocialsSection />
+      {/* <ContactSection /> */}
     </div>
   );
 };
@@ -69,7 +73,7 @@ const AboutSection = () => {
         technology and design.
       </motion.p>
       <motion.button
-        className={`bg-tertiary text-white py-4 px-8 rounded-lg font-bold text-lg mt-6`}
+        className={`bg-primary text-white py-4 px-8 rounded-lg font-bold text-lg mt-6`}
         initial={{ opacity: 0, y: 25 }}
         whileInView={{
           opacity: 1,
@@ -171,58 +175,123 @@ const ProjectSection = () => {
   return (
     <Section>
       <h2 className="text-6xl text-tertiary font-extrabold leading-snug">
-        Work
+        Projects
       </h2>
+      <motion.p
+        className="text-lg text-black mt-6"
+        initial={{ opacity: 0, y: 25 }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{ duration: 1, delay: 1.5 }}
+      >
+       Under construction...
+      </motion.p>
     </Section>
   );
 };
 
-const ContactSection = () => {
+const socials = [
+  {
+    title: "LinkedIn",
+    icon: linkedinIcon,
+    link: "https://www.linkedin.com/in/chelsea-quindipan/",
+  },
+  {
+    title: "GitHub",
+    icon: githubIcon,
+    link: "https://github.com/quindipc",
+  },
+  {
+    title: "Email",
+    icon: mailIcon,
+    link: "mailto:quindipc@gmail.com",
+  },
+];
+
+const SocialsSection = () => {
   return (
     <Section>
-      <h2 className="text-5xl font-bold">Contact me</h2>
-      <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
-        <form>
-          <label
-            htmlFor="name"
-            className="font-medium text-gray-900 block mb-1"
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-          />
-          <label
-            htmlFor="email"
-            className="font-medium text-gray-900 block mb-1 mt-8"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-          />
-          <label
-            htmlFor="email"
-            className="font-medium text-gray-900 block mb-1 mt-8"
-          >
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-          />
-          <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
-            Submit
-          </button>
-        </form>
+      <div>
+        <h2 className="text-6xl text-tertiary font-extrabold leading-snug">
+         Socials
+        </h2>
+        <div className="mt-8 space-y-">
+          <div className="grid grid-cols-3 gap-2">
+            {socials.map((social, index) => (
+              <div className="w-64" key={index}>
+                <a
+                  href={social.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                <motion.img
+                  src={social.icon}
+                  alt={social.title}
+                  className="w-20 h-20 m-2 transition-transform transform hover:scale-110"
+                  initial={{ opacity: 0 }}
+                  whileInView={{
+                    opacity: 1,
+                  }}
+                  transition={{ duration: 1, delay: 1 }}
+                />
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </Section>
   );
 };
+
+// const ContactSection = () => {
+//   return (
+//     <Section>
+//       <h2 className="text-5xl font-bold">Contact me</h2>
+//       <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
+//         <form>
+//           <label
+//             htmlFor="name"
+//             className="font-medium text-gray-900 block mb-1"
+//           >
+//             Name
+//           </label>
+//           <input
+//             type="text"
+//             name="name"
+//             id="name"
+//             className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+//           />
+//           <label
+//             htmlFor="email"
+//             className="font-medium text-gray-900 block mb-1 mt-8"
+//           >
+//             Email
+//           </label>
+//           <input
+//             type="email"
+//             name="email"
+//             id="email"
+//             className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+//           />
+//           <label
+//             htmlFor="email"
+//             className="font-medium text-gray-900 block mb-1 mt-8"
+//           >
+//             Message
+//           </label>
+//           <textarea
+//             name="message"
+//             id="message"
+//             className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+//           />
+//           <button className="bg-primary text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
+//             Submit
+//           </button>
+//         </form>
+//       </div>
+//     </Section>
+//   );
+// };
