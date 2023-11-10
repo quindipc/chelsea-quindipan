1842b1b07db9b1933e2060b61cb8f91a8d2779c8
Chelsea Quindipan <quindipc@gmail.com>
2023-11-10 12:30:56 -0500

    fix menu size

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -25,7 +25,7 @@ export const Menu = (props) => {
         </button>
         <div
           className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col
-        ${menuOpened ? "w-80" : "w-0"}`}
+        ${menuOpened ? "w-60" : "w-0"}`}
         >
           <div className="text-tertiary flex-1 flex items-start justify-center flex-col gap-6 p-8">
             <MenuButton label="About" onClick={() => onSectionChange(0)} />
