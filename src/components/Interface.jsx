86249a68a7654d7a9ea3448b2945f028c2eb8e42
Chelsea Quindipan <quindipc@gmail.com>
2023-11-14 13:39:44 -0500

    delete comments

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import reactIcon from "../assets/icons/react.png";
 import mysqlIcon from "../assets/icons/mysql.png";
 import nodeIcon from "../assets/icons/nodejs.png";
@@ -228,6 +228,20 @@ const SkillsSection = () => {
   );
 };
 
+
+const projects = [
+  {
+    title: "Project 1",
+    // image: "",
+    description: "Description for Project 1...",
+  },
+  {
+    title: "Project 2",
+      // image: "",
+    description: "Description for Project 2...",
+  },
+];
+
 const ProjectSection = () => {
   return (
     <Section>
@@ -248,91 +262,3 @@ const ProjectSection = () => {
     </Section>
   );
 };
-
-
-
-// const SocialsSection = () => {
-//   return (
-//     <Section>
-//       <div>
-//         <h2 className="text-6xl text-tertiary font-extrabold leading-snug">
-//          Socials
-//         </h2>
-//         <div className="mt-8 space-y-">
-//           <div className="grid grid-cols-3 gap-2">
-//             {socials.map((social, index) => (
-//               <div className="w-64" key={index}>
-//                 <a
-//                   href={social.link}
-//                   target="_blank"
-//                   rel="noopener noreferrer"
-//                 >
-//                 <motion.img
-//                   src={social.icon}
-//                   alt={social.title}
-//                   className="w-20 h-20 m-2 transition-transform transform hover:scale-110"
-//                   initial={{ opacity: 0 }}
-//                   whileInView={{
-//                     opacity: 1,
-//                   }}
-//                   transition={{ duration: 1, delay: 1 }}
-//                 />
-//                 </a>
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </Section>
-//   );
-// };
-
-// const ContactSection = () => {
-//   return (
-//     <Section>
-//       <h2 className="text-5xl font-bold">Contact me</h2>
-//       <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
-//         <form>
-//           <label
-//             htmlFor="name"
-//             className="font-medium text-gray-900 block mb-1"
-//           >
-//             Name
-//           </label>
-//           <input
-//             type="text"
-//             name="name"
-//             id="name"
-//             className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-//           />
-//           <label
-//             htmlFor="email"
-//             className="font-medium text-gray-900 block mb-1 mt-8"
-//           >
-//             Email
-//           </label>
-//           <input
-//             type="email"
-//             name="email"
-//             id="email"
-//             className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-//           />
-//           <label
-//             htmlFor="email"
-//             className="font-medium text-gray-900 block mb-1 mt-8"
-//           >
-//             Message
-//           </label>
-//           <textarea
-//             name="message"
-//             id="message"
-//             className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-//           />
-//           <button className="bg-primary text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
-//             Submit
-//           </button>
-//         </form>
-//       </div>
-//     </Section>
-//   );
-// };
