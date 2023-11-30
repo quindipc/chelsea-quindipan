b5a27b0ec3ab24cc4a3b489c32e13d8cdd824cb1
Chelsea Quindipan <quindipc@gmail.com>
2023-11-30 14:02:32 -0500

    edit avatar animation

---
+++ src/components/Background.jsx
@@ -0,0 +1,44 @@
+import { Sphere, useScroll } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+export const Background = () => {
+  const material = useRef();
+  const color = useRef({
+    color: "#E6E3DD",
+  });
+  const data = useScroll();
+
+  const tl = useRef();
+
+  useFrame(() => {
+    tl.current.progress(data.scroll.current);
+    material.current.color = new THREE.Color(color.current.color);
+  });
+
+  useEffect(() => {
+    tl.current = gsap.timeline();
+    tl.current.to(color.current, {
+      color: "#E6E3DD",
+    });
+    tl.current.to(color.current, {
+      color: "#E6E3DD",
+    });
+    tl.current.to(color.current, {
+      color: "#F8BCD3",
+    });
+  }, []);
+
+  return (
+    <group>
+      <Sphere scale={[30, 30, 30]}>
+        <meshBasicMaterial
+          ref={material}
+          side={THREE.BackSide}
+          toneMapped={false}
+        />
+      </Sphere>
+    </group>
+  );
+};
